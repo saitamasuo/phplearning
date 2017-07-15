7087ea4a137e77bb968e421897c7d41ad09005ba
kuroda akito <kuroda@kurodanoMacBook-Air.local>
2017-07-16 02:38:26 +0900

    初回コミット

---
+++ html/inquiry.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Shift_JIS">
+<title>お問い合わせフォーム</title>
+</head>
+<body>
+■お問い合わせ内容を入力してください。
+<br>
+<form
---
+++ html/last.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+<?php
+    echo('last php');
+?>
+</body>
+</html>
